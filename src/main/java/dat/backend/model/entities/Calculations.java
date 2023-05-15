ebce8b5eb4c78729e2d25c3e8286e72109bf3015
havvand <90390212+havvand@users.noreply.github.com>
2023-05-15 09:21:55 +0200

    Methods added: Methods added to Calculations (Methods for posts, stern and rafter done) Temp test class

--- src/main/java/dat/backend/model/entities/Calculations.java
+++ src/main/java/dat/backend/model/entities/Calculations.java
@@ -6,74 +6,72 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Calculations
-{
+public class Calculations {
 
-    public int calculateRafters(double lengthInCm) {
+    public int calculateRafters(double lengthInCm, double width) {
         final double RAFTER_SPACING_CM = 55;
-        double rafterCount = lengthInCm/RAFTER_SPACING_CM; // How many rafters can fit along the length of the carport.
-        double rafterValue = rafterCount - (int)rafterCount; // The decimal-numbers of the rafterCount. (1.818 x length (eg. 2,20m) = 3,99 rafters. )
-        int numberOfRafters = (int)rafterCount;
 
-        if(rafterValue >= 0.36) { // If the rafterValue is less than 0.36 (55cm x 0.364 = 20 cm), there will always be at least 20 cm to the last rafter.
-            return numberOfRafters;
+        double rafterCount = lengthInCm / RAFTER_SPACING_CM; // (Decimal) How many rafters will fit inside the roof.
+        double rafterValue = rafterCount - (int) rafterCount; // The decimal-points from rafterCount to calculate the excess distance.
+        int numberOfRafters = (int) rafterCount; // The whole number of rafters.
 
-        }else numberOfRafters = numberOfRafters-1;
+        if (rafterValue <= 0.36) { // If its below 0.36 the last rafter will be removed and the back-stern will act last rafter.
+            numberOfRafters = numberOfRafters - 1;
+        }
+
+        if (width <= 600 && width >= 481) {
+            System.out.println("RAFTERS 6 meters"); // TODO: Return the corretct rafter
+        } else if (width == 300) {
+            numberOfRafters = numberOfRafters/2;
+            System.out.println("RaFTERS 6 meters " + numberOfRafters); // TODO: Return the corretct rafter
+        } else if (width == 240) {
+            numberOfRafters = numberOfRafters/2;
+            System.out.println("Rafters 4.8 " + numberOfRafters); // TODO: Return the corretct rafter
+        } else System.out.println("Rafters 4.8");
 
         return numberOfRafters;
     }
 
+
     public int calculateRafterWidth() {
         //TODO: Make method that calculates the number of boards needed for the different widths of the carport.
-        return  0;
+        return 0;
     }
 
     public int calculatePosts(double lengthInCm) {
         int post = 0;
 
-        if(lengthInCm >= 481){
+        if (lengthInCm >= 481) {
             post = 6;
         } else post = 4;
 
         return post;
     }
 
-    public Map<Integer, String> calculateFrontAndBackSternBoards(double lengthInCm, int woodLength){
-        Map<Integer, String> sternBoards = new HashMap<>();
+    public Map<Integer, String> calculateFrontAndBackSternBoards(double lengthInCm) {
+        final double WOOD_LENGTH = 540;
+        //TODO: SHOUDL RETURN ARRAYLIST? WITH OBJECTS OF BOARDS - LONG OR SHORT
 
-        if((lengthInCm/woodLength) % 2 == 0) {
+        double woodNeeded = lengthInCm/WOOD_LENGTH;
+        double woodRatio = woodNeeded - (int)woodNeeded;
+        double wholeWood = woodNeeded-woodRatio;
+        double remainder = lengthInCm-(wholeWood*WOOD_LENGTH);
 
+        int woodToShip = 0;
+
+        if (woodNeeded == 4){
+            System.out.println("A okay");
+            woodToShip = (int)wholeWood;
+        } else if ((wholeWood*WOOD_LENGTH) < lengthInCm) {
+            System.out.println(remainder);
+            if (remainder <= 360){
+                System.out.println("Add 1 short");
+                woodToShip += wholeWood+1;
+            } else System.out.println("Add one long " + (woodToShip += wholeWood+1));
         }
+
+        System.out.println("Wood to ship " + woodToShip + " " + woodToShip*WOOD_LENGTH);
 
         return null;
     }
-
-    /*private int[][] readArrayFromFile()
-    {
-        double[][] array = new double[7][13];
-        try
-        {
-            Scanner myFileReader = new Scanner(new File("src/main/resources/Testetete.csv"));
-
-            int i = 0;
-            while (myFileReader.hasNextLine())
-            {
-
-                String line = myFileReader.nextLine();
-                String[] tokens = line.split(";");
-
-                for (int j = 0; j < tokens.length; j++)
-                {
-                    array[i][j] = Double.parseDouble(tokens[j]);
-                }
-                i++;
-            }
-        }
-        catch (FileNotFoundException e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }*/
-
 }
--- src/test/java/dat/backend/model/entities/CalculationsTest.java
+++ src/test/java/dat/backend/model/entities/CalculationsTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationsTest {
@@ -58,27 +61,147 @@ class CalculationsTest {
     @Test
     void calculateFrontAndBackSternBoards(){
 
-        double lengthInCm = 3120;
-        final double WOOD_LENGTH = 540;
+        double carportWidthInCm = 390; // Parameters
+        double carportLengthInCm = 390; // Parameters
 
-        double woodNeeded = lengthInCm/WOOD_LENGTH;
-        double woodRatio = woodNeeded - (int)woodNeeded;
-        double wholeWood = woodNeeded-woodRatio;
-        double remainder = lengthInCm-(wholeWood*WOOD_LENGTH);
+        //double woodShort = 360;
+        //double woodLong = 540;
 
-        int woodToShip = 0;
+        Map<String, Double> woodList = new HashMap<>();
+        woodList.put("woodShort", 360.00);
+        woodList.put("woodLong", 540.00);
 
-        if (woodNeeded == 4){
-            System.out.println("A okay");
-            woodToShip = (int)wholeWood;
-        } else if ((wholeWood*WOOD_LENGTH) < lengthInCm) {
-            System.out.println(remainder);
-            if (remainder <= 360){
-                System.out.println("Add 1 short");
-                woodToShip += wholeWood+1;
-            } else System.out.println("Add one long " + (woodToShip += wholeWood+1));
+        /*if(lengthInCm == 1440){
+            woodLength = 360;
+        }*/
+
+        double woodNeeded = 0;
+        double woodRatio = 0;
+        double wholeWood = 0;
+        double remainder;
+        double closeToOneWidth = 0;
+        double closeToOneLength = 0;
+        boolean yes = false;
+
+        for(Map.Entry<String, Double> set: woodList.entrySet()) { // Gets the piece of wood thats is cloesest to coerving the length
+            if(carportWidthInCm / set.getValue() >= closeToOneWidth){
+                System.out.println(set.getValue());
+                System.out.println("closetoWidth " + (carportWidthInCm) / set.getValue());
+                System.out.println(set.getValue());
+                closeToOneWidth = carportWidthInCm / set.getValue();
+                System.out.println(carportWidthInCm);
+            }
         }
 
-        System.out.println("Wood to ship " + woodToShip + " " + woodToShip*WOOD_LENGTH);
+        for(Map.Entry<String, Double> set: woodList.entrySet()) { // Gets the piece of wood thats is cloesest to coerving the length
+            if(carportLengthInCm / set.getValue() >= closeToOneLength){
+                System.out.println(set.getValue());
+                System.out.println("closetoLength " + (carportLengthInCm) / set.getValue());
+                System.out.println(set.getValue());
+                closeToOneLength = carportLengthInCm / set.getValue();
+                System.out.println(carportLengthInCm);
+            }
+        }
+
+        // All calculations are based on a single side.
+
+        for(Map.Entry<String, Double> set: woodList.entrySet()) { // Everything woodtype in Map is run through
+
+            if (carportWidthInCm <= 540) {
+                System.out.println("START SHORT" + closeToOneWidth);
+                if ((carportWidthInCm / set.getValue()) % 1 == 0) {
+                    System.out.println("SEND XX");
+                    if(set.getValue() == carportWidthInCm){ // set.getValue is length of wood.
+                        System.out.println("SHIP 2x WOOD EQUAL TO get.value " + set.getValue());
+                    }
+                } else if (set.getValue() == carportWidthInCm*2){
+                    System.out.println("SHIP 1x WOOD EQUAL TO GET VALUE " +  set.getValue()); // ONE WHOLE PIECE OF WOOD
+                } else if ((carportWidthInCm*2 / set.getValue()) >= closeToOneWidth && (carportWidthInCm / set.getValue()) < 1) { // Calculates the
+                    System.out.println("SENDT " + set.getKey()); // Mangler at få en lille med
+                    System.out.println(carportWidthInCm / set.getValue() + set.getKey());
+                }
+            } else if (carportWidthInCm >= 541 && (carportWidthInCm*2 / set.getValue()) < 2.5) { // last part makes sure only set.getValue is used.
+                System.out.println("2 x long " + "1 x short"); // Combined width is 1200 - so 2 x long 1 short
+                /*woodNeeded = (carportWidthInCm / set.getValue());
+                woodRatio = woodNeeded - (int) woodNeeded;
+                wholeWood = woodNeeded - woodRatio;
+                remainder = carportWidthInCm - (wholeWood * set.getValue());
+                System.out.println(woodNeeded);
+                System.out.println("re " + remainder);
+                if (remainder > 0 && remainder <= 180) {
+                    System.out.println("2 x Long and 1 Short");
+                }*/
+                // Can Add More Functionality for more wood or wider carport.
+            }
+        }
+
+        for(Map.Entry<String, Double> set: woodList.entrySet()) {
+            if (carportLengthInCm <= 540) {
+                System.out.println("START - Length " + closeToOneLength);
+                if ((carportLengthInCm / set.getValue()) % 1 == 0) {
+                    System.out.println("LENGTH SEND XX - Length");
+                    if (set.getValue() == carportLengthInCm) { // set.getValue is length of wood.
+                        System.out.println("LENGTH SHIP 2x WOOD EQUAL TO get.value " + set.getValue());
+                    }
+                } else if (set.getValue() == carportLengthInCm * 2) {
+                    System.out.println("LENGTH SHIP 1x WOOD EQUAL TO GET VALUE " + set.getValue()); // ONE WHOLE PIECE OF WOOD
+                } else if ((carportLengthInCm * 2 / set.getValue()) >= closeToOneLength && (carportLengthInCm / set.getValue()) < 1) { // Calculates the
+                    System.out.println("LENGTH SENDT " + set.getKey()); // Mangler at få en lille med
+                    System.out.println(carportLengthInCm / set.getValue() + set.getKey());
+                }
+            } else if (carportLengthInCm >= 541) {
+                System.out.println("LENGTH - 2 x long " + "1 x short");
+                /*woodNeeded = (carportWidthInCm / set.getValue());
+                woodRatio = woodNeeded - (int) woodNeeded;
+                wholeWood = woodNeeded - woodRatio;
+                remainder = carportWidthInCm - (wholeWood * set.getValue());
+                System.out.println(woodNeeded);
+                System.out.println("re " + remainder);
+                if (remainder > 0 && remainder <= 180) {
+                    System.out.println("2 x Long and 1 Short");
+                }*/
+                // Can Add More Functionality for more wood or wider carport.
+            }
+        }
+
+        /*if(carportLengthInCm <= 541){
+            if((carportLengthInCm/woodShort) % 1 == 0){
+                System.out.println("less 540 - 2 x Short");
+            }
+            else if((carportLengthInCm/woodLong) % 1 == 0){
+                System.out.println("540 - 2 x Long");
+            } else if((carportLengthInCm <= 720)){
+                System.out.println("less 720 - 4 x Short");
+            }
+        }*/
+    }
+
+    @Test
+    public void calculateRaftersTest() {
+        final double RAFTER_SPACING_CM = 55;
+        double lengthInCm = 780;
+        double width = 300;
+
+        double rafterCount = lengthInCm / RAFTER_SPACING_CM;
+        double rafterValue = rafterCount - (int) rafterCount;
+        int numberOfRafters = (int) rafterCount;
+        System.out.println(numberOfRafters);
+        if (rafterValue <= 0.36) { // If its below 0.36 the end of the carport will be the "last" rafter.
+            numberOfRafters = numberOfRafters - 1;
+        }
+
+        if (width <= 600 && width >= 481) {
+            System.out.println("RAFTERS 6 meters");
+        } else if (width == 300) {
+            numberOfRafters = numberOfRafters/2;
+            System.out.println("RaFTERS 6 meters " + numberOfRafters);
+        } else if (width == 240) {
+            numberOfRafters = numberOfRafters/2;
+            System.out.println("Rafters 4.8 " + numberOfRafters);
+        } else System.out.println("Rafters 4.8");
+
+        assertEquals(numberOfRafters, 1);
+
+
     }
 }
